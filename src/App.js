e703d7b1189622d39cd135a4cb66b4610079747e
KavyasriPechetti <kavyasripechetti@gmail.com>
2024-02-27 12:43:49 +0530

    formik(1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
 
 function App() {
+  var userForm=useFormik({
+    initialValues:{fn:"", ln:"", age:""},
+    onSubmit:(fv)=>{console.log(fv)}
+  })
+  // console.log(userForm);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Form Handling</h1>
+     <form onSubmit={userForm.handleSubmit}>
+      <input type='text' name="fn" placeholder='firstname' onChange={userForm.handleChange}/> <br/> <br/>
+      <input type='text' name='ln' placeholder='lastname' onChange={userForm.handleChange}/> <br/> <br/>
+      <input type='text' name='age' placeholder='age' onChange={userForm.handleChange}/> <br/> <br/>
+      <button type='submit'>Save</button>
+
+     </form>
     </div>
   );
 }
