7082899f3ad5b9ca11f78de58c5dbe2cd625e433
KavyasriPechetti <kavyasripechetti@gmail.com>
2024-03-01 12:56:18 +0530

    shopping cart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import Counter from './components/Counter';
-import Todolist from './components/Todolist';
 import Products from './components/products';
-import Cart from './components/cart';
 
 function App() {
   return (
     <Provider store={store}>
       <div >
-      {/* <Todolist></Todolist> */}
        
-        <Products></Products>
-        {/* <Counter></Counter> */}
-        
-       
-        
+        <Products></Products>    
       </div>
     </Provider>
 
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,15 +1,11 @@
-import counterReducer from "./counter.reducer";
-import studentReducer from "./student.reducer";
-import todoReducer from "./todo.reducer";
+
 import productReducer from "./product.reducer";
 import cartReducer from "./cart.reducer";
 import { combineReducers } from "redux";
 
 
 var reducer=combineReducers({
-    counter:counterReducer,
-    todolist:todoReducer,
-    students:studentReducer,
+    
     products:productReducer,
     cart:cartReducer
     
