c2772f02f1153c43fdae1ff6bf6623dea94fa0df
KavyasriPechetti <kavyasripechetti@gmail.com>
2024-03-05 14:49:28 +0530

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './home';
-import {Link} from "react-router-dom";
 
 function App() {
   return (
     <>
     
       <Navbar></Navbar>
-     
-
+    
       <Home></Home>
      
     </>
